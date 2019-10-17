f01d7e7021445b44d5186c4dd521dcb4fd89fac6
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2019-10-17 15:35:16 -0300

    falta relacionar usuario con productos

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :products
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  get 'users/search'
+  root to: 'products#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
